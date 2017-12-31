49b091bef1ee478139e49630f98d97590a8b5291
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-01-01 00:36:14 +0800

    solution 88-Merge-Sorted-Array

--- compiler/C/Main.c
+++ compiler/C/Main.c
@@ -2,49 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 typedef int bool;
-struct ListNode
-{
-  int val;
-  struct ListNode *next;
-};
-
-struct ListNode *deleteDuplicates(struct ListNode *head);
+void merge(int *nums1, int m, int *nums2, int n);
 int main()
 {
-  struct ListNode *list1 = (struct ListNode*)malloc(sizeof(struct ListNode));
-  struct ListNode *list2 = (struct ListNode*)malloc(sizeof(struct ListNode));
-  struct ListNode *list3 = (struct ListNode*)malloc(sizeof(struct ListNode));
-  struct ListNode *list4 = (struct ListNode *)malloc(sizeof(struct ListNode));
-  struct ListNode *list5 = (struct ListNode *)malloc(sizeof(struct ListNode));
-  list1->val=1;
-  list1->next=list2;
-  list2->val=1;
-  list2->next=list3;
-  list3->val=2;
-  list3->next = list4;
-  list4->val = 3;
-  list4->next = list5;
-  list5->val = 3;
-  list5->next = NULL;
-  struct ListNode *head=deleteDuplicates(list1);
-  while (head != NULL)
-  {
-    printf("%d->",head->val);
-    head=head->next;
-  }
+  int arr[]={1,0},arr2[]={2};
+  merge(arr,1,arr2,1);
 }
-struct ListNode *deleteDuplicates(struct ListNode *head){
-  struct ListNode *list = head;
-  while (list != NULL && list->next != NULL)
-  {
-    if (list->val == list->next->val)
-    {
-      list->next = list->next->next;
-    }
-    else
-    {
-      list = list->next;
+void merge(int *nums1, int m, int *nums2, int n)
+{
+  int i=m,p=n,j=0;
+  for(;i<m+n;i++)
+    nums1[i]=nums2[--p];
+  for(i=0;i<m+n;i++){
+    for(j=i+1;j<m+n;j++){
+      if(nums1[i]>nums1[j]){
+        int temp = nums1[i];
+        nums1[i]=nums1[j];
+        nums1[j]=temp;
+      }
     }
   }
-  return head;
 }
